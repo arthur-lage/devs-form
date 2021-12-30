5d88634c6ebd2e63b270b0f3cda03f6204e68ff9
Arthur Lage <arthurlage2006@gmail.com>
2021-12-30 07:40:59 -0300

    dev validation added

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,9 @@ const about = document.querySelector("#about");
 
 const submitButton = document.querySelector("#submit");
 
+const errorElement = document.querySelector("#error");
+const successElement = document.querySelector("#success");
+
 let technologiesArray = [];
 
 checkboxes.forEach((checkbox) => {
@@ -22,8 +25,50 @@ checkboxes.forEach((checkbox) => {
   });
 });
 
+const validateEmail = (email) => {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+  return false;
+};
+
 submitButton.addEventListener("click", async (e) => {
   e.preventDefault();
+
+  let messages = [];
+
+  if (name.value == "" || name.value == null) {
+    messages.push("Name is required");
+  }
+
+  if (surname.value == "" || surname.value == null) {
+    messages.push("Surname is required");
+  }
+
+  if (email.value == "" || email.value == null) {
+    messages.push("Email is required");
+  }
+
+  if (!validateEmail(email.value)) {
+    messages.push("Invalid email");
+  }
+
+  if (technologiesArray.length == 0) {
+    messages.push("You need to select at least 1 technology");
+  }
+
+  if (about.value == "" || about.value == null) {
+    messages.push("You need to write something here!");
+  }
+
+  if (messages.length > 0) {
+    errorElement.classList.add("visible");
+    errorElement.innerHTML = messages.join("</br>");
+    errorElement.scrollIntoView();
+    return;
+  } else {
+    errorElement.classList.remove("visible");
+  }
 
   developsFor = document.querySelector(".developsFor div input:checked");
 
@@ -48,4 +93,24 @@ submitButton.addEventListener("click", async (e) => {
       body: JSON.stringify(newUser),
     }
   );
+
+  if (res.status != 201) {
+    errorElement.classList.add("visible");
+    successElement.classList.remove("visible");
+
+    if (res.status == 409) {
+      errorElement.innerHTML = "This email is already being used!";
+    }
+
+    if (res.status == 500) {
+      errorElement.innerHTML = "An error occurred!";
+    }
+
+    errorElement.scrollIntoView();
+  } else {
+    successElement.classList.add("visible");
+    errorElement.classList.remove("visible");
+    successElement.innerHTML = "Dev successfully registered!";
+    errorElement.scrollIntoView();
+  }
 });
